a9253e207c8e5922e634af9c437d1193225f45d6
knysi3 <61900540+knysi3@users.noreply.github.com>
2023-01-29 13:11:43 +0900

    update

--- funcs.php
+++ funcs.php
@@ -6,11 +6,11 @@ function h($str)
 
 function db_conn(){
     try {
-      $db_name = "kon123_mil_practice_8";
-      $db_id   = "kon123";
-      $db_pw   = "Zaq12wsx";
-      $db_host = "mysql57.kon123.sakura.ne.jp";
-      $db_port = "3306";
+      $db_name = "xx";
+      $db_id   = "xx";
+      $db_pw   = "xx";
+      $db_host = "xx";
+      $db_port = "xx";
       $pdo = new PDO('mysql:dbname=' . $db_name . ';charset=utf8;host=' . $db_host.';port='.$db_port.'', $db_id, $db_pw);
       return $pdo;
     } catch (PDOException $e) {
